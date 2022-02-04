29f239bf68c9953a6022d3a7553f5137f5cc6f8c
Sari02 <90795239+Sari02@users.noreply.github.com>
2022-02-04 17:52:58 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,111 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Runner = Matter.Runner;
+
+var engine, world;
+var floor, meeseeks ;
+var entrada, salida;
+var plat1, plat2, plat3, plat4, plat5, plat6,plat7, plat8, plat9, plat10, plat11, platultima;
+var tostadora;
+var s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, smovil;
+
+function setup(){
+    var canvas = createCanvas(1800,900);
+    //engine = Matter.Engine.create();
+    engine = Engine.create();
+    world = engine.world;
+
+    
+    floor = new Floor(900,height,1800,30);
+    meeseeks = new Meeseeks(140,100);
+
+    //entrada = new Door(140,100,110,150);
+    salida = new Door(1740,100,110,150);
+
+    tostadora = new Plataforma(500,100,70,100);
+
+    plat1 = new Plataforma(140,180,140,20) ;
+    plat2 = new Plataforma(300,150,40,300);
+    plat3 = new Plataforma(200,700,140,20);
+    plat4 = new Plataforma(450,715,60,300);
+    plat5 = new Plataforma(660,650,140,20);
+    plat6 = new Plataforma(880,690,60,350);
+    plat7 = new Plataforma(1230,650,600,20);
+    plat8 = new Plataforma(1380,750,700,20);
+    plat9 = new Plataforma(1750,600,20,320);
+    plat10 = new Plataforma(1150,450,140,20);
+    plat11 = new Plataforma(1450,300,140,20);
+
+    platultima = new Plataforma(1720,180,150,20) ;
+
+    s1 = new Sierra(0,140,40,PI/2);
+    s2 = new Sierra(0,240,40,PI/2);
+    s3 = new Sierra(0,340,40,PI/2);
+    s4 = new Sierra(0,440,40,PI/2);
+
+    s5 = new Sierra(80,850,40,PI/2);
+    s6 = new Sierra(180,850,40,PI/2);
+    s7 = new Sierra(280,850,40,PI/2);
+
+    s8 = new Sierra(590,850,40,PI/2);
+    s9 = new Sierra(720,850,40,PI/2);
+    s10 = new Sierra(850,625,40,PI/2);
+    s11 = new Sierra(850,725,40,PI/2);
+
+    smovil = new Sierra(720,350,40,PI/2)
+   
+    Matter.Runner.run(engine);
+
+}
+
+
+function draw(){
+    background(0);
+    //Engine.update(engine);
+
+
+    //entrada.display();
+    //salida.display();
+
+    meeseeks.display();
+
+  
+    
+
+    tostadora.display();
+
+    plat1.display();
+    plat2.display();
+    plat3.display();
+    plat4.display();
+    plat5.display();
+    plat6.display();
+    plat7.display();
+    plat8.display();
+    plat9.display();
+    plat10.display();
+    plat11.display();
+    platultima.display();
+
+    floor.display();
+
+    s1.display();
+    s2.display();
+    s3.display();
+    s4.display();
+    s5.display();
+    s6.display();
+    s7.display();
+    s8.display();
+    s9.display();
+    s10.display();
+    s11.display();
+    smovil.display();
+    
+    //console.log(meeseeks.position.x);
+    //console.log(meeseeks.velocity);
+ 
+}
+
+
